767365c5404257a4571e9ddc0dc3ac72e09cfa2a
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-29 18:02:47 +0700

    Slowly working on it

--- Magic Squares.cpp
+++ Magic Squares.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main ()
 {
-	int i, j, x, y, z;
+	int i, j, x, y, z, n, check, sum[400], max = 0;
 	scanf("%d %d %d", &x, &y, &z);
 	int a[x][x], b[y][y], c[z][z];
+	
+	for(i=0; i<=400; i++)
+		sum[i] = 0;
 	
 	for(i=0; i<x; i++)
 	{
@@ -25,5 +28,33 @@ int main ()
 		{
 			scanf("%d", &c[i][j]);
 		}
+	}
+	
+	n = 0;
+	check = 1;
+	for(i=0; i<x; i++)
+	{
+		if(i == 0)
+		{
+			j = 0;
+			while(i<x)
+			{
+				sum[n]+=a[i][j];
+				i++;
+				j++;
+			}
+			i = 0;
+			j--;
+			n++;
+			while(j>=0)
+			{
+				sum[n]+=a[i][j];
+				i++;
+				j--;
+			}
+			j = 0;
+			n++;
+		}
+		
 	}
 }
