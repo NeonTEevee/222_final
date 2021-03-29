ba6eeccf3e23397098386d84ba6670e2bec367df
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-29 17:44:24 +0700

    Trying to fix things

--- Looblike.cpp
+++ Looblike.cpp
@@ -1,5 +1,31 @@
 #include <stdio.h>
 int main ()
 {
+	int i, n, x, max = 0, like[10000];
+	scanf("%d", &n);
 	
+	for(i=0; i<=10000; i++)
+		like[i] = 0;
+	
+	for(i=0; i<n; i++)
+	{
+		scanf("%d", &x);
+		like[x]+=1;
+		printf("Like[%d] = %d\n", x, like[x]);
+	}
+	
+	for(i=0; i<=10000; i++)
+	{
+		if(max < like[i])
+		{
+			max = like[i];
+			printf("Max = %d = %d\n", like[i], max);
+		}
+	}
+	
+	for(i=0; i<=10000; i++)
+	{
+		if(like[i] == max)
+			printf("%d ", i);
+	}
 }
