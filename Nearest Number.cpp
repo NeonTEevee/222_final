4132eda11e8ce0cdaa8923e36ed10e479b45bf78
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-29 17:18:07 +0700

    Wondering what to do

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -1,15 +1,22 @@
 #include <stdio.h>
 int main ()
 {
-	int i, j, k, n, check;
+	int i, j, k, n, check, ans;
 	scanf("%d %d", &k, &n);
 	int digit[5];
 	
 	j = 1;
 	check = 0;
-	for(i=10; i<k*10; i*=10)
+	for(i=10; i<=k*10; i*=10)
 	{
 		digit[check] = (k%i)/j;
 		j*=10;
+		check++;
 	}
+	
+	for(i=k; i<check; i++)
+	{
+		
+	}
+	
 }
