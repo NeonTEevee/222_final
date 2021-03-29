799c523dccadb68d8dcefbdaaed1b3d0770323e7
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-29 17:24:29 +0700

    Trying to fix things

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-	int i, j, k, n, check, ans;
+	int i, j, k, n, m, check, ans;
 	scanf("%d %d", &k, &n);
 	int digit[5];
 	
@@ -12,11 +12,27 @@ int main ()
 		digit[check] = (k%i)/j;
 		j*=10;
 		check++;
+		printf("Digit[%d] = %d\n", check, digit[check-1]);
 	}
 	
-	for(i=k; i<check; i++)
+	digit[0]++;
+	m = 1;
+	for(i=k+1; i<32750; i++)
 	{
-		
+		for(j=10; j<=i*10; j*=10)
+		{
+			digit[check] = (i%j)/m;
+			m*=10;
+			check++;
+		}
+		for(j=0; j<5; j++)
+		{
+			if(digit[j] = n)
+			{
+				printf("%d", i);
+				return 0;
+			}
+		}
 	}
 	
 }
