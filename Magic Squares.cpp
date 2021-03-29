49e5d189ef4082e97311ca1a89c3a9afebb87954
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-29 18:17:48 +0700

    Still working

--- Magic Squares.cpp
+++ Magic Squares.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-	int i, j, x, y, z, n, m, check, sum[400], max = 0;
+	int i, j, x, y, z, n, m, check, sum[400], num;
 	scanf("%d %d %d", &x, &y, &z);
 	int a[x][x], b[y][y], c[z][z];
 	
@@ -30,7 +30,7 @@ int main ()
 		}
 	}
 	
-	n = 0;
+	n = 0;							//A
 	check = 1;
 	for(i=0; i<x; i++)
 	{
@@ -45,6 +45,7 @@ int main ()
 			}
 			i = 0;
 			j--;
+			num = sum[n];
 			n++;
 			while(j>=0)
 			{
@@ -69,5 +70,119 @@ int main ()
 		}
 		n++;
 	}
-	max = n;
+	
+	for(i=0; i<n; i++)
+	{
+		if(sum[i] != num)
+			check = 0;
+	}
+	if(check == 0) {
+		printf("no\n");
+	} else {
+		printf("yes\n");
+	}
+	
+	n = 0;							//B
+	check = 1;
+	for(i=0; i<y; i++)
+	{
+		if(i == 0)
+		{
+			j = 0;
+			while(i<y)
+			{
+				sum[n]+=b[i][j];
+				i++;
+				j++;
+			}
+			i = 0;
+			j--;
+			num = sum[n];
+			n++;
+			while(j>=0)
+			{
+				sum[n]+=b[i][j];
+				i++;
+				j--;
+			}
+			j = 0;
+			n++;
+		}
+		for(j=0; j<y; j++)
+		{
+			sum[n]+=b[i][j];
+		}
+		n++;
+	}
+	for(j=0; j<y; j++)
+	{
+		for(i=0; i<y; i++)
+		{
+			sum[n]+=b[i][j];
+		}
+		n++;
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		if(sum[i] != num)
+			check = 0;
+	}
+	if(check == 0) {
+		printf("no\n");
+	} else {
+		printf("yes\n");
+	}
+	
+	n = 0;							//C
+	check = 1;
+	for(i=0; i<z; i++)
+	{
+		if(i == 0)
+		{
+			j = 0;
+			while(i<z)
+			{
+				sum[n]+=c[i][j];
+				i++;
+				j++;
+			}
+			i = 0;
+			j--;
+			num = sum[n];
+			n++;
+			while(j>=0)
+			{
+				sum[n]+=c[i][j];
+				i++;
+				j--;
+			}
+			j = 0;
+			n++;
+		}
+		for(j=0; j<z; j++)
+		{
+			sum[n]+=c[i][j];
+		}
+		n++;
+	}
+	for(j=0; j<z; j++)
+	{
+		for(i=0; i<z; i++)
+		{
+			sum[n]+=c[i][j];
+		}
+		n++;
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		if(sum[i] != num)
+			check = 0;
+	}
+	if(check == 0) {
+		printf("no\n");
+	} else {
+		printf("yes\n");
+	}
 }
